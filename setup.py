cecb997bfdb60c1b5dab2611570834060db461d0
Евгений <pomponchik@MacBook-Pro-Evgenij.local>
2023-04-12 20:12:08 +0400

    work in progress

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ setup(
     version="0.0.1",
     author="Evgeniy Blinov",
     author_email="zheni-b@yandex.ru",
-    description="Lazy f-strings",
+    description="Lazy f-strings for everyone",
     long_description=readme,
     long_description_content_type="text/markdown",
     url="https://github.com/pomponchik/fazy",

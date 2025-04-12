5511ee4dab3b895956524c44822158ac3342ae54
Евгений Блинов <pomponchik@Mac-mini-Evgenij.local>
2025-04-12 19:15:11 +0300

    deleted all AST-related things from the code

--- f/proxy_module.py
+++ f/proxy_module.py
@@ -1,6 +1,5 @@
 import gc
 import sys
-import ast
 import inspect
 from string import Formatter
 from types import CodeType, FrameType
@@ -20,7 +19,7 @@ class SizedAndIterable(Sized, Iterable[Any], Protocol):
 class ProxyModule(sys.modules[__name__].__class__):  # type: ignore[misc]
     old_str = str
 
-    def __call__(self, string: Union[LazyString, str], lazy: bool = True, safe: bool = True, closures: bool = True) -> Union[LazyString, str]:
+    def __call__(self, string: Union[LazyString, str], lazy: bool = True, closures: bool = True) -> Union[LazyString, str]:
         if isinstance(string, LazyString):
             return string
 
@@ -32,9 +31,8 @@ class ProxyModule(sys.modules[__name__].__class__):  # type: ignore[misc]
             {**base_frame.f_globals},
             self.sum_of_nonlocals(
                 base_frame.f_back,
-                self.get_qualname(base_frame.f_code, raise_if_not_literal=safe, code_line=base_frame.f_lineno),
+                self.get_qualname(base_frame.f_code, code_line=base_frame.f_lineno),
                 closures,
-                safe,
             ),
             lazy,
         )
@@ -49,7 +47,7 @@ class ProxyModule(sys.modules[__name__].__class__):  # type: ignore[misc]
     def __repr__(self) -> str:
         return 'f'
 
-    def sum_of_nonlocals(self, first_frame: Optional[FrameType], base_qualname: Optional[str], closures: bool, safe: bool) -> Dict[str, Any]:
+    def sum_of_nonlocals(self, first_frame: Optional[FrameType], base_qualname: Optional[str], closures: bool) -> Dict[str, Any]:
         if not closures or first_frame is None or base_qualname is None:
             return {}
 
@@ -57,7 +55,7 @@ class ProxyModule(sys.modules[__name__].__class__):  # type: ignore[misc]
         while first_frame is not None:
             code = first_frame.f_code
 
-            qualname = self.get_qualname(code, raise_if_not_literal=False, code_line=0)
+            qualname = self.get_qualname(code, code_line=0)
             if qualname is not None:
                 if self.startswith(base_qualname.split('.'), qualname.split('.')):
                     all_locals.append(first_frame.f_locals)
@@ -74,7 +72,7 @@ class ProxyModule(sys.modules[__name__].__class__):  # type: ignore[misc]
         return result
 
     @classmethod
-    def get_qualname(cls: Type['ProxyModule'], code: CodeType, raise_if_not_literal: bool, code_line: int) -> Optional[str]:
+    def get_qualname(cls: Type['ProxyModule'], code: CodeType, code_line: int) -> Optional[str]:
         functions = []
 
         for function in gc.get_referrers(code):
@@ -88,64 +86,7 @@ class ProxyModule(sys.modules[__name__].__class__):  # type: ignore[misc]
 
         if functions:
             function = functions[0]
-            if raise_if_not_literal:
-                cls.check_code(function, code, code_line)
             return function.__qualname__  # type: ignore[no-any-return]
-
-    @staticmethod
-    def check_code(function: Callable[..., Any], code: CodeType, code_line: int) -> None:
-        try:
-            if inspect.isgenerator(function):
-                code_strings, begin_code_line_number = inspect.getsourcelines(code)
-            else:
-                code_strings, begin_code_line_number = inspect.getsourcelines(function)
-        except Exception:
-            return
-
-        spaces_count = 0
-        for letter in code_strings[0]:
-            if not letter.isspace():
-                break
-            spaces_count += 1
-
-        code_strings = [x[spaces_count:] for x in code_strings]
-
-        full_code = ''.join(code_strings)
-        ast_of_code = ast.parse(full_code)
-
-        flag: Union[int, bool] = True
-        class ConstantVisitor(ast.NodeVisitor):
-            def visit_Call(self, node: ast.Call) -> None:
-                nonlocal flag
-                if node.lineno + begin_code_line_number - 1 == code_line:
-                    if hasattr(node.func, 'id') and node.func.id == 'f':
-                        if len(node.args) == 1 and isinstance(node.args[0], ast.Constant):
-                            flag *= True
-                        elif len(node.args) == 1 and isinstance(node.args[0], ast.Call) and hasattr(node.args[0].func, 'id') and node.args[0].func.id == 'f':
-                            flag *= True
-                            for arg in node.args:
-                                ConstantVisitor().visit(arg)
-                            for keyword in node.keywords:
-                                ConstantVisitor().visit(keyword.value)
-                        else:
-                            flag *= False
-                    elif isinstance(node.func, ast.Attribute):
-                        ConstantVisitor().visit(node.func.value)
-                        for arg in node.args:
-                            ConstantVisitor().visit(arg)
-                        for keyword in node.keywords:
-                            ConstantVisitor().visit(keyword.value)
-                    else:
-                        for arg in node.args:
-                            ConstantVisitor().visit(arg)
-                        for keyword in node.keywords:
-                            ConstantVisitor().visit(keyword.value)
-
-        ConstantVisitor().visit(ast_of_code)
-
-        if not flag and not (sys.version_info < (3, 8)):
-            raise SyntaxError('Unsafe use of a variable as a template.')
-
 
     @staticmethod
     def startswith(iterable: SizedAndIterable, second_iterable: SizedAndIterable) -> bool:
@@ -157,14 +98,3 @@ class ProxyModule(sys.modules[__name__].__class__):  # type: ignore[misc]
                 return False
 
         return True
-
-    def get_all_mentioned_names(self, string: str) -> Set[str]:
-        result = set()
-
-        class Visiter(ast.NodeVisitor):
-            def visit_Name(_self, node: ast.AST) -> Optional[ast.AST]:
-                result.add(node.id)
-
-        Visiter().visit(ast.parse(string))
-
-        return result

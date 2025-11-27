4abb2493f9b0872f14bf72654e89f1017062d485
Евгений Блинов <pomponchik@Mac-mini-Evgenij.local>
2025-11-28 02:43:24 +0300

    Add logging file content print to test
    ```python with open(file_name, 'r') as file: content = file.read()
    print(repr(content)) ```

--- tests/test_init.py
+++ tests/test_init.py
@@ -248,6 +248,10 @@ def test_logging_to_file():
     file_name = os.path.join('tests', 'data', 'file.log')
     logging.root.addHandler(logging.FileHandler(file_name))
 
+    with open(file_name, 'r') as file:
+        content = file.read()
+        print(repr(content))
+
     logging.error(f('kek'))
 
     with open(file_name, 'r') as file:
